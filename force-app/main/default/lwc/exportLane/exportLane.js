14ed9f58c4bcb15cee948a5f5cd02dabdf3eb115
adityanaag3 <atopalli@salesforce.com>
2021-06-25 22:03:35 +0530

    Added Download button

--- force-app/main/default/lwc/exportLane/exportLane.js
+++ force-app/main/default/lwc/exportLane/exportLane.js
@@ -3,13 +3,17 @@ import exportLaneJSON from "@salesforce/apex/KickboardCtrl.exportLaneJSON";
 
 export default class ExportLane extends LightningElement {
     @api recordId;
-    data;
+    dataUrl;
     error;
+
+    get downloadFileName() {
+        return `Lane_${this.recordId}`;
+    }
 
     @wire(exportLaneJSON, { laneId: "$recordId" })
     jsonStr({ data, error }) {
         if (data) {
-            this.data = data;
+            this.dataUrl = `data:application/json,${data}`;
         } else if (error) {
             this.error = error;
         }
