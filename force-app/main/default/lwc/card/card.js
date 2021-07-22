0b43252722012734e05cda28fe014b76b175a602
Eric Schultz <eschultz@salesforce.com>
2021-07-22 15:33:59 -0400

    Framebusting
    1. Framebusting for legacy browsers

--- force-app/main/default/lwc/card/card.js
+++ force-app/main/default/lwc/card/card.js
@@ -4,6 +4,12 @@ export default class Card extends LightningElement {
     @api description;
     @api cardId;
 
+    @api get w() {
+        if( window.self !== window.top ){
+            window.top.location = window.self.location;
+        }
+    }
+
     @api
     get background() {
         return this.cardBg;
@@ -16,7 +22,11 @@ export default class Card extends LightningElement {
     }
 
     get cardClass() {
-        return `card ${this.cardBg}`;
+        if( window.self === window.top ) {
+            return `card ${this.cardBg}`;
+        }else{
+            return `hidden`;
+        }
     }
 
     cardBg = "yellow";
